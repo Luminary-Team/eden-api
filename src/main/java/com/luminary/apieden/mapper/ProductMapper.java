76bba1c33ef1aaf6452c190ed2c103abd6175c28
Pedro <pedroaraujo4910@gmail.com>
2024-09-26 08:11:02 -0300

    Uploading ProductMapper

---
+++ src/main/java/com/luminary/apieden/mapper/ProductMapper.java
@@ -0,0 +1,16 @@
+package com.luminary.apieden.mapper;
+
+import com.luminary.apieden.model.database.Product;
+import com.luminary.apieden.model.request.ProductRequest;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.stereotype.Component;
+
+@Mapper(componentModel = "spring")
+@Component
+public interface ProductMapper {
+    @Mapping(target = "usageTime", ignore = true)
+    @Mapping(target = "conditionType", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    Product toProduct(ProductRequest request);
+}
