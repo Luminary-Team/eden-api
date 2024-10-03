19feef9a2ff75fb3871624df3af258c14b603dd7
Pedro <pedroaraujo4910@gmail.com>
2024-10-03 11:19:54 -0300

    Fixing Rating.java validation messages

--- src/main/java/com/luminary/apieden/model/database/Rating.java
+++ src/main/java/com/luminary/apieden/model/database/Rating.java
@@ -1,10 +1,10 @@
 package com.luminary.apieden.model.database;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
@@ -22,25 +22,32 @@ public class Rating {
     @Id
     @Column(name = "pk_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(hidden = true)
     private long id;
 
     @Column(name = "rating")
     @Min(message = "O campo 'rating' não pode ser abaixo de 0", value = 0)
+    @Schema(name = "rating", description = "The respective value of the rating", example = "3.5")
     private float rating;
 
     @OneToOne
     @JoinColumn(name = "fk_user_appraiser_id")
     @NotNull(message = "O campo 'userAppraiser' precisa ser passado")
+    @Schema(name = "userAppraiser", description = "The User appraiser information", implementation = User.class)
     private User userAppraiser;
 
     @OneToOne
     @JoinColumn(name = "fk_products_id")
+    @Schema(name = "product", description = "The product information", implementation = Product.class)
     private Product product;
 
-    @Column(name = "fk_ecopoints_id")
-    private int ecopointsId;
+    @OneToOne
+    @JoinColumn(name = "fk_ecopoints_id")
+    @Schema(name = "ecopoint", description = "The ecopoint information", implementation = Ecopoint.class)
+    private Ecopoint ecopoint;
 
     @OneToOne
     @JoinColumn(name = "fk_users_evaluated_id")
-    private User usersEvaluatedId;
+    @Schema(name = "ecopoint", description = "The ecopoint information", implementation = Ecopoint.class)
+    private User userEvaluated;
 }
