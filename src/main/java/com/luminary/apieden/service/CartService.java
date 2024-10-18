f9b88d43ec7330b8ef2706170b05b1c050dc77c1
Pedro <pedroaraujo4910@gmail.com>
2024-10-18 12:16:13 -0300

    Updating to return totalSale

--- src/main/java/com/luminary/apieden/service/CartService.java
+++ src/main/java/com/luminary/apieden/service/CartService.java
@@ -1,13 +1,17 @@
 package com.luminary.apieden.service;
 
 import com.luminary.apieden.mapper.CartMapper;
+import com.luminary.apieden.model.database.Cart;
 import com.luminary.apieden.model.database.CartItem;
 import com.luminary.apieden.model.database.Product;
 import com.luminary.apieden.model.exception.HttpError;
 import com.luminary.apieden.model.procedure.TotalSaleProcedure;
 import com.luminary.apieden.model.request.CartItemRequest;
 import com.luminary.apieden.model.response.CartItemResponse;
+import com.luminary.apieden.model.response.FindCartItemResponse;
+import com.luminary.apieden.model.response.RegisterCartItemResponse;
 import com.luminary.apieden.repository.CartItemRepository;
+import com.luminary.apieden.repository.CartRepository;
 import com.luminary.apieden.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,21 +24,28 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 public class CartService {
+    private final CartRepository cartRepository;
     private final CartItemRepository cartItemRepository;
     private final ProductRepository productRepository;
     private final TotalSaleProcedure totalSaleProcedure;
     private final CartMapper cartMapper;
-    public List<CartItemResponse> findCartItemsByCartId(String cartId) {
+    public FindCartItemResponse findCartItemsByCartId(String cartId) {
         log.info("Finding cartItems by cart id: {}", cartId);
         List<CartItem> cartItemList = cartItemRepository.findCartItemsByCartId(Long.parseLong(cartId));
-        return cartItemList.stream()
+        List<CartItemResponse> cartItemResponseList = cartItemList.stream()
                 .map(cartItem -> {
                     Product product = productRepository.findById(cartItem.getProductId()).get();
-                    return cartMapper.toRegisterCartItem(cartItem, product);
+                    return cartMapper.toCartItemResponse(cartItem, product);
                 })
                 .toList();
+        Cart cart = cartRepository.findById(Long.parseLong(cartId))
+                .orElseThrow(() -> {
+                    log.error("[Cart Service] Cart could not be found to return totalSale");
+                    return new HttpError(HttpStatus.INTERNAL_SERVER_ERROR, "Não conseguiu fazer a consulta para totalSale.");
+                });
+        return cartMapper.toFindCartItemResponse(cartItemResponseList, cart.getTotalSale());
     }
-    public CartItemResponse register(CartItemRequest request) {
+    public RegisterCartItemResponse register(CartItemRequest request) {
         log.info("Finding product described in cardItem.productId. ID: {}", request.getProductsId());
         Product product = productRepository.findById(request.getProductsId())
                 .orElseThrow(() -> new HttpError(HttpStatus.BAD_REQUEST, "Produto não encontrado"));
@@ -50,7 +61,7 @@ public class CartService {
                 .build();
         totalSaleProcedure.totalSale((int) (request.getCartId()));
         cartItemRepository.save(cartItem);
-        return cartMapper.toRegisterCartItem(cartItem, product);
+        return cartMapper.toRegisterCartItemResponse(cartItem, product);
     }
     public void delete(String cartItemId) {
         cartItemRepository.deleteById(Long.valueOf(cartItemId));
