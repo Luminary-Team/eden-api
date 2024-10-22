307290c4445ec6237bcb7d422fe768faf5aea2a9
Pedro <pedroaraujo4910@gmail.com>
2024-10-21 22:50:20 -0300

    Removing ProductRepository findProductByPremium

--- src/main/java/com/luminary/apieden/repository/ProductRepository.java
+++ src/main/java/com/luminary/apieden/repository/ProductRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findProductsByPremium(boolean premium);
     List<Product> findByUserId(long id);
     List<Product> findByTitleLike(String title);
     List<Product> findProductsByUserIdNotAndPremiumIsTrue(long userId);
