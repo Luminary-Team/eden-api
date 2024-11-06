b02fdd99d872bc844de2a04cd79faf63e4dafca2
Pedro <pedroaraujo4910@gmail.com>
2024-11-06 19:06:26 -0300

    Implementing neo4j call to create relationships

--- src/main/java/com/luminary/apieden/client/Neo4jClient.java
+++ src/main/java/com/luminary/apieden/client/Neo4jClient.java
@@ -1,13 +1,19 @@
 package com.luminary.apieden.client;
 
+import com.luminary.apieden.model.client.CreateRelationshipRequest;
+import com.luminary.apieden.model.client.CreateRelationshipResponse;
 import com.luminary.apieden.model.client.CreateUserRequest;
 import com.luminary.apieden.model.client.CreateUserResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+
 @FeignClient(url = "${api.neo4j}", name = "neo4jClient")
 public interface Neo4jClient {
     @PostMapping("/user")
     CreateUserResponse createUser(@RequestBody CreateUserRequest request);
+    @PostMapping("/purchase")
+    List<CreateRelationshipResponse> createRelationship(@RequestBody List<CreateRelationshipRequest> request);
 }
